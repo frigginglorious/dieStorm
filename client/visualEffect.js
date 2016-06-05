bade7cc593507c69b7f0fb0ab932e3264d46b29a
frigginglorious <frigginglorious@gmail.com>
2016-06-05 14:18:06 -0500

    sexy stylize with js and css

--- client/visualEffect.js
+++ client/visualEffect.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
 
     $('.aDie').click(function(){
-        // $this.addClass('spin');
         var spin = $(this);
-        console.log($(this));
         spin.addClass('spin');
         setTimeout(function (){
-            console.log('time start');
             spin.removeClass('spin');
-        }, 500);
-        // $(this).removeClass('spin');
+        }, 1500);
     });
 });
